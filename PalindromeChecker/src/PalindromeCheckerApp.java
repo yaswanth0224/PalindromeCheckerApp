ebbe3d6d34d59ef6875425f2c91e8942ade087c1
yaswanth0224 <cheepuriyaswanth0224@gmail.com>
2026-02-23 15:40:31 +0530

    usecase1 added

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -1,3 +1,28 @@
-public class PalindromeCheckerApp{
+public class PalindromeCheckerApp {
 
+    public static void main(String[] args) {
+
+        // ===== UC1: Welcome Message =====
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully.");
+        System.out.println();
+
+        // ===== UC2: Hardcoded Palindrome Check =====
+        String input = "madam";
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome");
+        } else {
+            System.out.println(input + " is NOT a Palindrome");
+        }
+    }
 }
